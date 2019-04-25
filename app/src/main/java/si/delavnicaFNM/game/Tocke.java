15646cd24e50e261d42c07dacbec92cdba7cc9a0
Nejc <dev.neetsu@gmail.com>
2019-04-26 00:00:35 +0200

    Coment update and format Tocke.java

--- app/src/main/java/si/delavnicaFNM/game/Tocke.java
+++ app/src/main/java/si/delavnicaFNM/game/Tocke.java
@@ -1,17 +1,10 @@
 package si.delavnicaFNM.game;
 
-import java.util.Vector;
-
-import org.w3c.dom.Text;
-
-import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.Button;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class Tocke extends Dialog implements OnClickListener{
@@ -39,21 +32,4 @@ public class Tocke extends Dialog implements OnClickListener{
 			this.dismiss();
 		}
 	}
-	
-	/*
-	public void izpisTock() {
-				TextView izpisText = (TextView) findViewById(R.id.izpis);
-//				for (int i=0; i<stEkip; i++){
-//					izpisText.setText(ekipe.get(i).vrniIme() + ": " + ekipe.get(i).vrniTocke();
-//				}
-				izpisText.setText("BananaFighters" + ": " + "120");
-				izpisText.setText("KiviShock" + ": " + "350");
-				izpisText.setText("LovelyKittens" + ": " + "210");
-				izpisText.setText("GreenPie" + ": " + "170");
-				izpisText.setText("YoMamma" + ": " + "80");
-		
-	}
-*/
-	
-
 }
